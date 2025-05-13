3e32515d0942702ce808f02d484403243c68b731
brandonhach <specialcambo@gmail.com>
2025-05-13 11:58:57 -0400

    [chore] initialize home page component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { CtaComponent } from '../../components/cta/cta.component';
+import { FaqComponent } from '../../components/faq/faq.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroComponent, CtaComponent, FaqComponent, FooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
