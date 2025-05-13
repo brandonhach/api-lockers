2d88bf2bf276fd9dcd7bb87227c22f7fa6891a01
brandonhach <specialcambo@gmail.com>
2025-05-13 12:10:01 -0400

    [chore] home page will only have top navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,17 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { CtaComponent } from '../../components/cta/cta.component';
 import { FaqComponent } from '../../components/faq/faq.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, CtaComponent, FaqComponent, FooterComponent],
+  imports: [
+    HeroComponent,
+    CtaComponent,
+    FaqComponent,
+    FooterComponent,
+    NavbarComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
